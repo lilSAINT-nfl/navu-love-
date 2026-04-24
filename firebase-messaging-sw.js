4840baf0db67a9b711a072001f325212c911a578
lilSAINT-nfl <pranav7.choudhari@gmail.com>
2026-04-25 00:11:30 +0530

    Set up Firebase messaging service in SW file
    Initialize Firebase app and messaging service.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,12 @@
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js');
 
+firebase.initializeApp({
+  apiKey: "AIzaSyAUrzZfBLbDOKYjtcmUZiCl9jPq3Vpib8o",
+  authDomain: "navu-love.firebaseapp.com",
+  projectId: "navu-love",
+  messagingSenderId: "843475296751",
+  appId: "1:843475296751:web:b4672a6746eb611264561a"
+});
+
+const messaging = firebase.messaging();
